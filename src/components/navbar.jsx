850fe912e79fe868a924ddc0239c9d4ed9ee6e73
Rashed-46 <earnestfaisal@gmail.com>
2026-01-21 13:09:52 +0600

    final touch

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -53,7 +53,11 @@ function Navbar() {
             </li>
             <li>
               {/* this file should be updated */}
-              <a href="https://docs.google.com/viewer?url=https://my-portfolio-i0vr9zj0h-rashed-46s-projects.vercel.app/resume.pdf" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/rashed-miah/my-portfolio/releases/download/v1.0/resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <span>
                   <FontAwesomeIcon icon={faFileLines} />
                 </span>
