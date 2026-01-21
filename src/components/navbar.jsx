f9268f2e4b0c4f6e4d11d61daaef13f6285aac24
Rashed-46 <earnestfaisal@gmail.com>
2026-01-21 13:48:20 +0600

    abr khaisi error

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,7 +54,7 @@ function Navbar() {
             <li>
               {/* this file should be updated */}
               <a
-                href="https://drive.google.com/file/d/1XRzEnA9Nh4PmU3orikE-CZsIxBHHu066/preview"
+                href="https://drive.google.com/file/d/1jRtAWsnmtiH3_yoAE6JMf0aonK2XY6nt/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
